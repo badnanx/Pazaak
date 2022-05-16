855a1695aeea7c436c12fa074329fcda803dc624
badnanx <98980519+badnanx@users.noreply.github.com>
2022-05-16 14:54:03 -0700

    changed Player class variable playerScore to just score

--- src/main/java/com/team3/cs210finalprojectteam3/Player.java
+++ src/main/java/com/team3/cs210finalprojectteam3/Player.java
@@ -2,7 +2,7 @@ package com.team3.cs210finalprojectteam3;
 
 public class Player {
 
-    private int playerScore;
+    private int score;
     private int roundsWon;
 
     private boolean hasStood;
@@ -15,7 +15,7 @@ public class Player {
 
     public Player()
     {
-        playerScore = 0;
+        score = 0;
         roundsWon = 0;
         hasStood = false;
         hasEndedTurn = false;
