935fa861cfda8452afec7c24c0800b0a7ad00d1a
badnanx <98980519+badnanx@users.noreply.github.com>
2022-05-16 16:12:23 -0700

    GameLogic additions: Added checkForBust() method Added but commented out compareScores() method (its incomplete)

--- src/main/java/com/team3/cs210finalprojectteam3/GameLogic.java
+++ src/main/java/com/team3/cs210finalprojectteam3/GameLogic.java
@@ -1,9 +1,12 @@
 package com.team3.cs210finalprojectteam3;
 
-// just testing a commit! 10:46 pm
+
 public class GameLogic {
 
+    private final int BUST_LIMIT = 20;
     private int whoseTurnIsIt;
+    boolean endOfRound; // necessary?
+
 
    // constructor method, creates new game
     public GameLogic()
@@ -11,16 +14,16 @@ public class GameLogic {
         // fresh game
         whoseTurnIsIt = 1; // starting with player 1; 2 means player 2
         System.out.println("DEBUG: Starting new game with player 1 goes first.");
-
+        endOfRound = false;
     }
 
+    // tracks whose turn it is;
     public void endTurn(int currentPlayer)
     {
         if(currentPlayer == 1)
         {
             whoseTurnIsIt = 2;
             System.out.println("DEBUG: Currently player 2's turn.");
-
         }
         else
         {
@@ -28,6 +31,47 @@ public class GameLogic {
             System.out.println("DEBUG: Currently player 1's turn.");
         }
     }
+
+    // checks for bust
+    public boolean checkForBust(int score)
+    {
+        if (score > BUST_LIMIT)
+        {
+            // System.out.println("DEBUG: Player busts.");
+            return true;
+        }
+        else
+            return false;
+    }
+
+
+    /**
+     * Code below is incomplete. Edits to Player class also required.
+     */
+
+//    public void compareScores(int p1Score, int p2Score)
+//    {
+//        if(p1Score == p2Score)
+//        {
+//            System.out.println("Draw game");
+//            // suggestion: popup indicating draw
+//            endOfRound = true; // necessary? why not stay in game loop?
+//        }
+//        else if (p1Score > p2Score)
+//        {
+//            p1.roundsWon++;
+//            System.out.println("Player 1 wins the round");
+//
+//            endOfRound = true;
+//        }
+//        else if(p1Score < p2Score)
+//        {
+//            p2.roundsWon++;
+//            System.out.println("Player 2 wins the round");
+//
+//            endOfRound = true;
+//        }
+//    }
 
 
 // make into singleton?
