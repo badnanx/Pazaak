ade14d254f826c1b9b9b2cef38f60ecb5649282d
badnanx <98980519+badnanx@users.noreply.github.com>
2022-05-17 11:47:59 -0700

    Deck.java: Made method that generates random draw card of range [1-10].

--- src/main/java/com/team3/cs210finalprojectteam3/Deck.java
+++ src/main/java/com/team3/cs210finalprojectteam3/Deck.java
@@ -14,7 +14,7 @@ public class Deck {
      * Generate a new Card with random values and return the card to be used elsewhere
      * @return
      */
-    public Card generateRandomCard(){
+    public Card generateRandomCard(){ // this is for hand cards; consider renaming: generateRandomHandCard
         Random rand = new Random();
         int randomNum = rand.nextInt((max - min) + 1) + min;
         while (randomNum == 0){
@@ -24,4 +24,24 @@ public class Deck {
         card.SetText(Integer.toString(randomNum));
         return card;
     }
+
+
+    /**
+     * Generate a new Card with random values [1-10] and return the card to be played to field.
+     * Should be able to use this method to randomly generate the cards
+     * to be played to the appropriate playing field at the start of a player's turn.
+     *
+     * Someone please review! Thank you.
+     * @return
+     */
+    public Card generateRandomDrawCard() {
+        Random rand = new Random(); //instance of random class
+        final int upperbound = 10;
+        //generate random values from 1-10
+        int int_random = rand.nextInt(upperbound) + 1;
+        Card card = new Card(int_random);
+        card.SetText(Integer.toString(int_random));
+        return card;
+    }
+
 }
