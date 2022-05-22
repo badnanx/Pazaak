83a858afc98f81a0d1697701f7e37bdbea6a4531
Bernbark <korystennett@yahoo.com>
2022-05-22 04:05:14 -0700

    Added Junit tests for making sure that hands are fully randomized when constructed.

--- src/test/java/PazaakTests.java
+++ src/test/java/PazaakTests.java
@@ -1,5 +1,9 @@
+import com.team3.cs210finalprojectteam3.Card;
 import com.team3.cs210finalprojectteam3.GameLogic;
+import com.team3.cs210finalprojectteam3.Hand;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -49,6 +53,40 @@ public class PazaakTests {
         boolean result = GameLogic.getInstance().checkForBust(-4);
         System.out.println("DEBUG: checkForBust result: " + result);
         assertFalse(result);
+    }
+
+    /**
+     * This test checks to make sure that the Hand constructor randomizes a new list of cards for the hand
+     * every time it is called.
+     *
+     * @author Kory Stennett
+     */
+    @Test
+    void noSameHand(){
+        Hand hand = new Hand();
+        List<Card> handOneList = hand.hand;
+        hand = new Hand();
+        List<Card> handTwoList = hand.hand;
+        boolean result = handOneList.equals(handTwoList);
+        System.out.println("DEBUG: Two Lists of cards created from the same hand randomized twice are equal: "+result);
+        assertFalse(handOneList.equals(handTwoList));
+    }
+
+    /**
+     * This test is to essentially prove that noSameHand() is properly testing equality of hand lists.
+     *
+     * It should pass because both lists are made from the same hand. The important part is that
+     * equals() works for checking equality of lists.
+     * @author Kory Stennett
+     */
+    @Test
+    void sameHand(){
+        Hand hand = new Hand();
+        List<Card> handOneList = hand.hand;
+        List<Card> handTwoList = hand.hand;
+        boolean result = handOneList.equals(handTwoList);
+        System.out.println("DEBUG: Two Lists of cards created from the same hand are equal: "+result);
+        assertTrue(handOneList.equals(handTwoList));
     }
 }
 
