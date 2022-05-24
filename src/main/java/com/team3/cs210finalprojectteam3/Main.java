521779eb4535ec0978e32d02afab46c437ce3428
f-nding <iddunnoman@gmail.com>
2022-05-23 17:59:12 -0700

    Welcome Screen UI Update

--- src/main/java/com/team3/cs210finalprojectteam3/Main.java
+++ src/main/java/com/team3/cs210finalprojectteam3/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
 
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("welcome-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 610, 410);
         stage.setTitle("Pazaak");
         stage.setScene(scene);
         stage.show();
